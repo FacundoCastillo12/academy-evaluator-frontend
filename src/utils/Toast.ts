c5ce2e877263a0247588fcb6e249b90177fae128
FacundoCastillo12 <facuelfacha15@gmail.com>
2024-03-17 13:35:59 -0300

    Add new Toast class with toastify notification

---
+++ src/utils/Toast.ts
@@ -0,0 +1,31 @@
+import { Bounce, toast } from 'react-toastify';
+
+export class ToastNotify {
+  static sucess(message: string) {
+    toast.success(message, {
+      position: 'top-center',
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: 'dark',
+      transition: Bounce,
+    });
+  }
+
+  static error(message: string) {
+    toast.error(message, {
+      position: 'top-center',
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: 'dark',
+      transition: Bounce,
+    });
+  }
+}
